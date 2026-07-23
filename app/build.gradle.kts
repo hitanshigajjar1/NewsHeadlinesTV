4734d3ed81459ecc64977f5b87484035cd5b04f9
hitanshigajjar1 <hitanshikgajjar@gmail.com>
2026-07-24 01:37:11 +0530

    Initial Android TV project setup and folder structure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,35 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation(platform("androidx.compose:compose-bom:2025.06.00"))
+
+    implementation("androidx.activity:activity-compose:1.10.1")
+
+    implementation("androidx.compose.ui:ui")
+
+    implementation("androidx.compose.material3:material3")
+
+    implementation("androidx.compose.ui:ui-tooling-preview")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    implementation("androidx.core:core-ktx:1.16.0")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.2")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.2")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+
+    implementation("androidx.tv:tv-material:1.0.1")
+
+    implementation("androidx.tv:tv-foundation:1.0.1")
+
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    implementation("io.coil-kt:coil-compose:2.7.0")
 }
